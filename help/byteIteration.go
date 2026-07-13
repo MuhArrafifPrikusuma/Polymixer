4b2a5d51f61d7ca2674193d5288f0ee4ad02355a
MuhArrafifPrikusuma <muhammadarrafif1308@gmail.com>
2026-07-13 11:08:07 +0800

    byte iteration logic (still thinking) or probably not thinking at all my brain is fkin fried

--- help/byteIteration.go
+++ help/byteIteration.go
@@ -6,39 +6,75 @@ import (
 	"os"
 )
 
+// "PolyMixer/messages"
+// "bytes"
+// "os"
+
 // TODO: use this function later to cut the head of and metadata of pdf file and then also check the unused space between those two and use it to embed the whole mp3 binary there
-func search_and_count(lookFor, startFrom string, ignoreStartingValue bool, file *os.File) (offset int) {
-	fileStat, err := file.Stat()
+// func search_and_count(lookFor, startFrom string, ignoreStartingValue bool, file *os.File) (offset int) {
+// 	fileStat, err := file.Stat()
+// 	if err != nil {
+// 		messages.E_stat_read(err)
+// 	}
+//
+// 	buf_pdf := make([]byte, fileStat.Size())
+// 	_, err = file.ReadAt(buf_pdf, 0)
+// 	if err != nil {
+// 		messages.E_read(err)
+// 	}
+//
+// 	startingIndex := bytes.Index(buf_pdf, []byte(startFrom))
+// 	if startingIndex == -1 {
+// 		messages.E_index("startingIndex")
+// 	}
+//
+// 	sliced_buf_pdf := buf_pdf
+// 	if ignoreStartingValue {
+// 		sliced_buf_pdf = buf_pdf[startingIndex:]
+// 	}
+// 	endIndex := bytes.Index(sliced_buf_pdf, []byte(lookFor))
+// 	if endIndex == -1 {
+// 		messages.E_index("endIndex")
+// 	}
+//
+// 	// TODO: remind me to count offset and empty spaces between startIndex and endIndex later on to be used as
+// 	offset = 0
+// 	for i := startingIndex; i < endIndex; offset++ {
+// 	}
+//
+// 	return
+// }
+
+func Find_xref(f *os.File) {
+	target := []byte("xref")
+	fileStat, err := f.Stat()
+	defer f.Close()
 	if err != nil {
 		messages.E_stat_read(err)
 	}
 
-	buf_pdf := make([]byte, fileStat.Size())
-	_, err = file.ReadAt(buf_pdf, 0)
-	if err != nil {
-		messages.E_read(err)
-	}
+	buf := make([]byte, fileStat.Size())
 
-	startingIndex := bytes.Index(buf_pdf, []byte(startFrom))
-	if startingIndex == -1 {
-		messages.E_index("startingIndex")
-	}
+	xref_startIdx := bytes.Index(buf, target)
+	byteSlice := buf[:xref_startIdx]
 
-	sliced_buf_pdf := buf_pdf
-	if ignoreStartingValue {
-		sliced_buf_pdf = buf_pdf[startingIndex:]
-	}
-	endIndex := bytes.Index(sliced_buf_pdf, []byte(lookFor))
-	if endIndex == -1 {
-		messages.E_index("endIndex")
-	}
+	Find_last_obj_idx_and_last_obj_id(xref_startIdx, &byteSlice)
+}
 
-	// TODO: remind me to count offset and empty spaces between startIndex and endIndex later on to be used as
-	offset = 0
-	space_between := 0
-	for i := startingIndex; i < endIndex; offset++ {
+func Find_last_obj_idx_and_last_obj_id(xref_start_idx int, byteSlice *[]byte) {
+	target := []byte("obj")
+	sliceLen := len(*byteSlice)
+	n := 100
+	sliceStart := sliceLen - n
+	currentSlice := (*byteSlice)[sliceStart:sliceLen]
+	currentLen := len(currentSlice)
 
-	}
+	// TODO: find a way to search for target and make sure that there is only 2 obj at max with one as the starting obj and other endobj right before xref
+}
 
-	return
+// TODO: find last xref then iterate back to find last endobj then find the last obj id then append
+// to last_endobj_idx + 1 start with 1010 and end with 1010 places all of the data inside obj stream and
+// don't reference it on xref
+func Find_places_for_newObj(last_endobj_idx, last_obj_id int) {
+
 }
