901f0d9f30d485c6a7499585ae702436d84f81cc
MuhArrafifPrikusuma <muhammadarrafif1308@gmail.com>
2026-07-27 12:41:53 +0800

    preparing mp4 support

--- files/argument.go
+++ files/argument.go
@@ -21,7 +21,7 @@ func TakeArg(arg *Arguments) {
 	}
 
 	var lastObjId, cutToIdx, objSize int
-	var ptrMp3, newObj, bsfXref, ptrPdf *[]byte
+	var ptrMp, newObj, bsfXref, ptrPdf *[]byte
 	var fileType, fname string
 
 	if len(os.Args) >= 4 {
@@ -31,7 +31,7 @@ func TakeArg(arg *Arguments) {
 		}
 
 		arg.File1 = file
-		fileType, lastObjId, cutToIdx, ptrMp3, ptrPdf, _, bsfXref = getHeader(arg.File1)
+		fileType, lastObjId, cutToIdx, ptrMp, ptrPdf, _, bsfXref = getHeader(arg.File1)
 		messages.S_open_file(arg.File1, fileType)
 
 		file, err = os.Open(os.Args[2])
@@ -40,7 +40,7 @@ func TakeArg(arg *Arguments) {
 			messages.E_open_file(os.Args[2], err)
 		}
 		arg.File2 = file
-		fileType, tmplastObjId, tmpcutToIdx, tmpptrMp3, tmpptrPdf, _, tmpbsxfXref := getHeader(arg.File2)
+		fileType, tmplastObjId, tmpcutToIdx, tmpptrMp, tmpptrPdf, _, tmpbsxfXref := getHeader(arg.File2)
 
 		fname = os.Args[3]
 
@@ -49,32 +49,37 @@ func TakeArg(arg *Arguments) {
 			cutToIdx = tmpcutToIdx // <- this will be used for mixing
 			ptrPdf = tmpptrPdf
 			bsfXref = tmpbsxfXref
-		} else if fileType == "MP3" {
-			ptrMp3 = tmpptrMp3
+		} else if fileType == "MP3" || fileType == "MP4" {
+			ptrMp = tmpptrMp
 		}
 		messages.S_open_file(arg.File2, fileType)
-		newObj, objSize = Create_audio_object(ptrMp3, lastObjId)
+		newObj, objSize = Create_audio_video_object(ptrMp, lastObjId)
 	}
 	newXrefSlice := StartXref_refOffset(bsfXref, objSize)
 	Mix_MP3_and_PDF(ptrPdf, newXrefSlice, newObj, cutToIdx, fname)
 }
 
-func getHeader(file *os.File) (strReturn string, lastObjId, cutToIdx int, ptrMp3, ptrPdf *[]byte, objRefPtr *Xref_ObjMap_t, bsfXref *[]byte) {
-	buffer := make([]byte, 4)
+func getHeader(file *os.File) (strReturn string, lastObjId, cutToIdx int, ptrMp, ptrPdf *[]byte, objRefPtr *Xref_ObjMap_t, bsfXref *[]byte) {
+	buffer := make([]byte, 10)
 
 	_, err := file.ReadAt(buffer, 0)
 	if err != nil {
 		messages.E_read(err)
 	}
+	isMp4 := bytes.Index(buffer[:], []byte("ftyp"))
 	if bytes.HasPrefix(buffer, []byte("ID3")) {
-		ptrMp3 = mp3_get_body(file)
+		ptrMp = mp3_get_body(file)
 		strReturn = "MP3"
-		return strReturn, 0, 0, ptrMp3, nil, nil, nil
+		return strReturn, 0, 0, ptrMp, nil, nil, nil
 
 	} else if bytes.HasPrefix(buffer, []byte("%PDF")) {
 		lastObjId, cutToIdx, ptrPdf, objRefPtr, bsfXref = Pdf_open(file)
 		strReturn = "PDF"
 		return strReturn, lastObjId, cutToIdx, nil, ptrPdf, objRefPtr, bsfXref
+	} else if isMp4 != -1 {
+		ptrMp = mp4_read(file)
+		strReturn = "MP4"
+		return strReturn, 0, 0, ptrMp, nil, nil, nil
 	}
 	return "unknown file type", 0, 0, nil, nil, nil, nil
 }
